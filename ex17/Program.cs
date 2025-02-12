2f53061a5b591a9090e96cd944468e94045ad748
Joao miranda <joaomiranda.201932@epff-intep.pt>
2025-02-12 10:48:11 +0000

    Add files via upload

---
+++ ex17/Program.cs
@@ -0,0 +1,26 @@
+﻿public class execercio17
+{
+    public static void Main(String[] args)
+    {
+        // cria um array da lista de filmes
+        String[] listadefilmes = { "gato das bota","o icrivel mundo de jack", "harry Potter"};
+
+        //cria um array da lista de filmes
+        int[] anosdofilmes = { 1990, 2010, 2014 };
+        Console.WriteLine("Lista de filmes");
+        Console.WriteLine();
+
+        
+        for (int contador  = 0; contador < listadefilmes.Length ; contador++)
+        {
+            Console.WriteLine(listadefilmes[contador] + " - " + anosdofilmes[contador]);
+        
+        }
+
+
+
+
+        }
+
+}
+
---
+++ ex18/Program.cs
@@ -0,0 +1,26 @@
+﻿public class execercio18
+{
+    public static void Main(String[] args)
+    {
+        // cria um array da lista de cidades
+        String[] cidades = { "lisboa", "porto", "coimbra", "faro", "braga" };
+
+        //cria um array da lista de polucao
+        int[] populacao = { 545000, 237000, 143000, 118000, 193000 };
+        Console.WriteLine("cidades e as suas populacao");
+        Console.WriteLine();
+
+
+        for (int contador = 0; contador < cidades.Length; contador++)
+        {
+            Console.WriteLine(cidades[contador] + " - " + populacao[contador] + " Habitantes");
+
+        }
+
+
+
+
+    }
+
+}
+
